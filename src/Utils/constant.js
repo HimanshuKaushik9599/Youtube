e000ae418857990f4d990f2587a97afa636acaee
Himanshukaushik9599 <himanshukaushik9599@gmail.com>
2024-06-10 22:42:21 +0530

    add firebase

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -1,5 +1,4 @@
- export const GOOGLE_API_KEY="AIzaSyAuDEqIlkM2yra5qK2lXSY4RaUzykR21yo";
-export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
-export const YOUTUBE_SEARCH_API="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
-export const YOUTUBE_SUGGESTION_SEARCH_API="https://www.googleapis.com/youtube/v3/search?key=AIzaSyAuDEqIlkM2yra5qK2lXSY4RaUzykR21yo&q=iphone&part=snippet&maxResults=10"
-// https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyAuDEqIlkM2yra5qK2lXSY4RaUzykR21yo
+ export const GOOGLE_API_KEY=process.env.REACT_APP_GOOGLE_API_KEY;
+export const YOUTUBE_VIDEOS_API=process.env.REACT_APP_YOUTUBE_VIDEOS_API+GOOGLE_API_KEY;
+export const YOUTUBE_SEARCH_API=process.env.REACT_APP_YOUTUBE_SEARCH_API;
+export const YOUTUBE_SUGGESTION_SEARCH_API=process.env.REACT_APP_YOUTUBE_SUGGESTION_SEARCH_API;
